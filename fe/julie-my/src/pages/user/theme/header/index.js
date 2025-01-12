3a145844d4da2fa8a5c407e5ce04603758037db8
NTHieu23 <tdahieudz23@gmail.com>
2025-01-13 05:06:07 +0700

    done b4

--- fe/julie-my/src/pages/user/theme/header/index.js
+++ fe/julie-my/src/pages/user/theme/header/index.js
@@ -1,11 +1,65 @@
 import { memo } from "react";
 import "../header/style.scss";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { fomater } from "../../../../utils/fomater";
 
 const Header = () => {
   return (
     <>
-      <div className="top_header">Haaa</div>
-      <h1 className="container">Header</h1>
+      <div className="header_top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header_top_left">
+              <ul>
+                  <li>
+                  <AiOutlineMail/>
+                    hello@gmail.com
+                  </li>
+                  <li>
+                    miễn phí ship đơn từ {fomater(200000)}
+                  </li>
+              </ul>
+            </div>
+            <div className="col-6 header_top_right">
+              <ul>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineFacebook />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineLinkedin />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineTwitter />
+                  </Link>
+                  <span>
+                    <Link to={""}>
+                      <AiOutlineUser />
+                    </Link>
+                    Đăng Nhập
+                  </span>
+                </li>
+              </ul>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
